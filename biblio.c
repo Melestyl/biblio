39c3d17a978aa5f0826e2cd566cb5cce6dd67624
HmBloqued <timothee.laffra@gmail.com>
2022-02-10 11:18:18 +0100

    Better afficherBiblio

--- biblio.c
+++ biblio.c
@@ -19,11 +19,11 @@ int ajouterLivre(T_Bibliotheque *ptrB)
 
 int afficherBibliotheque(const T_Bibliotheque *ptrB)
 {
-    printf("\nAffichage de la bibliothèque :\n");
+    printf("\nAffichage de la bibliothèque :\n\n");
     if (ptrB->nbLivres == 0)
         return 1;
     for(int i=0; i < ptrB->nbLivres; i++) {
-        printf("\n\n-----------------\n");
+        printf("-----------------\n");
         printf("Titre : %s\n", ptrB->etagere[i].titre);
         printf("Auteur : %s\n", ptrB->etagere[i].auteur);
     }
