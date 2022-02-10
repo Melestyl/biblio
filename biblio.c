24725381e6ce0227b36ca59058a0940003e778f2
Melestyl <tommy.tuto@outlook.fr>
2022-02-10 10:53:41 +0100

    format + init

--- biblio.c
+++ biblio.c
@@ -1,20 +1,19 @@
 #include "biblio.h"
 
-void init (T_Bibliotheque *ptrB)
+void init(T_Bibliotheque *ptrB)
 {
-    
+    ptrB->nbLivres = 0;
+    return;
 }
 
-
-int ajouterLivre(T_Bibliotheque  *ptrB)
+int ajouterLivre(T_Bibliotheque *ptrB)
 {
-    
+
     return 0;
 }
 
-
-int  afficherBibliotheque(const T_Bibliotheque  *ptrB)
+int afficherBibliotheque(const T_Bibliotheque *ptrB)
 {
-    
+
     return 0;
 }
--- livre.c
+++ livre.c
@@ -1,17 +1,15 @@
 #include "livre.h"
 
-
-void saisirLivre(T_livre * ptrL)
+void saisirLivre(T_livre *ptrL)
 {
-lireChaine("TITRE :", (ptrL->titre), MAX_TITRE );
-lireChaine("AUTEUR :", (ptrL->auteur ), MAX);
+	lireChaine("TITRE :", (ptrL->titre), MAX_TITRE);
+	lireChaine("AUTEUR :", (ptrL->auteur), MAX);
 }
 
 void afficherLivre(const T_livre *ptrL)
 {
-afficherChaine("TITRE :", (ptrL->titre));
-printf(" - ");
-afficherChaine("AUTEUR :", (ptrL->auteur ));
-printf("\n");
+	afficherChaine("TITRE :", (ptrL->titre));
+	printf(" - ");
+	afficherChaine("AUTEUR :", (ptrL->auteur));
+	printf("\n");
 }
-
