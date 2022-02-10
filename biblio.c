a79db58c5010dc6415938b139c764cf49d57e423
HmBloqued <timothee.laffra@gmail.com>
2022-02-10 11:06:48 +0100

    made afficherBilio

--- biblio.c
+++ biblio.c
@@ -14,6 +14,11 @@ int ajouterLivre(T_Bibliotheque *ptrB)
 
 int afficherBibliotheque(const T_Bibliotheque *ptrB)
 {
-
+    int i; //Boucleur
+    printf("\nAffichage de la bibliothèque :\n");
+    for(i = 0; i < ptrB->nbLivres; i++)
+        printf("\n\n-----------------\n");
+        printf("Titre : %s\n", ptrB->etagere[i].titre);
+        printf("Auteur : %s\n", ptrB->etagere[i].auteur);
     return 0;
 }
